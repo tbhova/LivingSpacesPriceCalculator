8c00ddabe16b3aa2ba30560672385108b76f8684
Tyler Hovanec <tbhovanec@gmail.com>
2017-08-20 09:49:49 -0700

    Added button and onClick listeners for button and date input box

--- app/src/main/java/tbhova/livingspacespricecalculator/MainActivity.java
+++ app/src/main/java/tbhova/livingspacespricecalculator/MainActivity.java
@@ -1,5 +1,7 @@
 package tbhova.livingspacespricecalculator;
 
+import android.app.DatePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +10,23 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+import static android.text.InputType.TYPE_NULL;
 
 public class MainActivity extends AppCompatActivity {
+    private Button calculateButton;
+    private EditText priceInput, dateInput;
+    private TextView finalPrice;
+    private DatePickerDialog dateUpdatedPicker;
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy", Locale.US);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +41,39 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        calculateButton = (Button)findViewById(R.id.caclulatePriceButton);
+        priceInput = (EditText)findViewById(R.id.DisplayedPrice);
+        dateInput = (EditText)findViewById(R.id.UpdatedDate);
+        dateInput.setInputType(TYPE_NULL);
+        finalPrice = (TextView)findViewById(R.id.FinalPriceValue);
+
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String price = priceInput.getText().toString();
+                String date = priceInput.getText().toString();
+
+            }
+=        });
+
+        final Context mainContext = this;
+        dateInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+
+                dateUpdatedPicker = new DatePickerDialog(mainContext, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        Calendar newDate = Calendar.getInstance();
+                        newDate.set(year, month, dayOfMonth);
+                        dateInput.setText(dateFormat.format(newDate.getTime()));
+                    }
+                },
+                calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
             }
         });
     }
